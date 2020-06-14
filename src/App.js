781aa1480ce9acbffd22ded6893d45ddfbd5c819
Manoj <Manoj.Maduri@mckesson.com>
2020-06-14 15:00:24 -0400

    First commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import Table from "./Table";
+// import logo from "./logo.svg";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    const heading = <h1>Hello, React!</h1>;
+    const characters = [
+      {
+        name: "Charlie",
+        job: "Janitor",
+      },
+      {
+        name: "Mac",
+        job: "Bouncer",
+      },
+      {
+        name: "Dee",
+        job: "Aspring actress",
+      },
+      {
+        name: "Dennis",
+        job: "Bartender",
+      },
+    ];
+    return (
+      <div className="container">
+        <header className="h1 text-center">{heading}</header>
+        <Table characterData={characters} />
+      </div>
+    );
+  }
+}
+
+export default App;
