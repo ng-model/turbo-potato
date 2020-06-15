bc67c7fa14d1488c1c38a80d154b3bc31b803ab8
Manoj <Manoj.Maduri@mckesson.com>
2020-06-15 12:32:40 -0400

    Updates

---
+++ precache-manifest.d872f2b136369fc9f51f17b9d776e7b0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "772ff7793e595eed5e534ee2189a22c0",
+    "url": "/turbo-potato/index.html"
+  },
+  {
+    "revision": "3bcb3bcbae79a0c2352d",
+    "url": "/turbo-potato/static/css/2.74189dbb.chunk.css"
+  },
+  {
+    "revision": "edd71e8c9aca4747be24",
+    "url": "/turbo-potato/static/css/main.b81aef37.chunk.css"
+  },
+  {
+    "revision": "3bcb3bcbae79a0c2352d",
+    "url": "/turbo-potato/static/js/2.728b9dd5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/turbo-potato/static/js/2.728b9dd5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edd71e8c9aca4747be24",
+    "url": "/turbo-potato/static/js/main.32eef347.chunk.js"
+  },
+  {
+    "revision": "20f1ff72f3bd50a0a056",
+    "url": "/turbo-potato/static/js/runtime-main.73dc4472.js"
+  }
+]);
